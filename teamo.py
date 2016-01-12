8aea4b982dde3d9c4961b8501d1177b3640f456e
Jason K. Moore <moorepants@gmail.com>
2016-01-11 17:20:40 -0800

    Beginnings of creating a ranking cost function for projects.

--- teamo.py
+++ teamo.py
@@ -1,5 +1,145 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
+
+import numpy as np
+
+"""
+
+Given a list of project preferences for each student we need to form teams
+such that:
+
+1. Each team is guaranteed to have at least one common project.
+2. Each team is assigned a unique project.
+3. Each team should have a unique set of students.
+4. The assigned project should be weighted to the top choice(s) of the team
+    members.
+
+cost function: 0-1
+
+For no matches in a team: 0
+Other: # matches / # team mates
+At least one in each team: 1
+
+Cost: # matches / # team mates
+
+# top choices / # team mates
+
+Constraint:
+
+    team 1 proj != team 2 proj != team 3 proj != team N proj
+
+"""
+
+
+def match_cost(team_array, team_choices):
+    """
+
+    N : number of teams
+    n : maximum number of teammates (can include nan)
+
+    team_array : ndarray of integers, shape(N, n)
+    team_choices : dictionary
+
+    """
+
+
+    number_of_teams = team_array.shape[0]
+
+    counts = match_counts(team_array, team_choices)
+
+    cost = 0
+    for team, matches in counts.items():
+        cost += max(matches.values()) / len(team)
+
+    return cost / number_of_teams
+
+
+def convert_to_int(team_array):
+
+    nan_idx = np.isnan(team_array)
+    team_array = team_array.astype(np.int64)
+    team_array[nan_idx] = 9999
+
+    return team_array
+
+
+def choose_projects(team_array, team_choices, project_list):
+    """Returns a unique project assignment for each team."""
+
+    project_assignments = {}
+
+    counts = match_counts(team_array, team_choices)
+
+    for team, matches in counts.items():
+        for project, val in matches.items():
+            if val == max(matches.values()):
+                while True:
+                    try:
+                        project_list.remove(project)
+                    except ValueError:
+                        continue
+                    proj_sel = project
+                break
+        project_assignments[proj_sel] = team
+
+    return project_assignments
+
+
+def match_counts(team_array, team_choices):
+    """Returns a count of the project matches for each team."""
+
+    team_array = convert_to_int(team_array)
+
+    # TODO : This can probably use colletions.Counter in some to make it
+    # faster.
+
+    # If there is a student listed in the team_array that doesn't exist in the
+    # team_choices then convert to nan.
+    students = team_choices.keys()
+    for i, team in enumerate(team_array):
+        for j, student in enumerate(team):
+            if student not in students:
+                team_array[i, j] = 9999
+
+    counts = {}
+    for team in team_array:
+        team_without_nans = tuple(team[team != 9999])
+        counts[team_without_nans] = defaultdict(lambda: 0)
+        for student in team_without_nans:
+            for choice in team_choices[student]:
+                counts[team_without_nans][choice] += 1
+
+    return counts
+
+
+def unique_students(team_array):
+    """Returns 0 if list of students is unique and 1 if not. Ignores nans."""
+
+    students = convert_to_int(team_array).flatten()
+
+    # TODO : remove any students that are equal to 9999
+
+    if len(students) > len(set(students)):
+        return 1
+    else:
+        return 0
+
+
+def unique_projects(team_array, projects):
+    """Returns 0 if the list of projects is unique and 1 if not."""
+
+    if len(projects) > len(set(projects)):
+        return 1
+    else:
+        return 0
+
+
+def equality_constraints(team_array, project_list):
+
+    return np.array([unique_students(team_array),
+                     unique_projects(team_array, project_list)])
+
 
 class MultipleChoiceSingleAnswerQuestion(object):
 
--- test_teamo.py
+++ test_teamo.py
@@ -1,4 +1,134 @@
-from teamo import MultipleChoiceSingleAnswerQuestion
+import numpy as np
+
+from teamo import (match_counts, unique_students, unique_projects,
+                   equality_constraints, choose_projects,
+                   MultipleChoiceSingleAnswerQuestion)
+
+
+def test_match_counts():
+
+    team_array = np.array([[0, 1, 2, np.nan],
+                           [3, 4, 5, 6]])
+
+    choices = {0: ['a', 'b', 'c'],
+               1: ['c', 'd', 'e'],
+               2: ['b', 'c', 'a'],
+               3: ['f', 'g', 'h', 'i'],
+               4: ['a', 'f', 'h'],
+               5: ['c', 'f', 'e'],
+               6: ['c', 'a', 'e']}
+
+    expected_counts = {
+                    (0, 1, 2): {'a': 2, 'b': 2, 'c': 3, 'd': 1, 'e': 1},
+                    (3, 4, 5, 6): {'a': 2, 'c': 2, 'e': 2, 'f': 3, 'g': 1,
+                                   'h': 2, 'i': 1}
+                   }
+
+    counts = match_counts(team_array, choices)
+
+    assert counts == expected_counts
+
+    # This should effectively ignore the student 10 which is not in the
+    # choices.
+    team_array = np.array([[0, 1, 2, 10],
+                           [3, 4, 5, 6]])
+
+    counts = match_counts(team_array, choices)
+
+    assert counts == expected_counts
+
+
+def test_unique_students():
+
+    students = np.array([[0, 1, 2, np.nan],
+                         [3, 4, 5, 6]])
+
+    assert unique_students(students) == 0
+
+    students = np.array([[0, 1, 2, np.nan],
+                         [3, 4, 2, 6]])
+
+    assert unique_students(students) == 1
+
+    students = np.array([[0, 1, 2, 9999],
+                         [3, 4, 5, 6]])
+
+    assert unique_students(students) == 0
+
+    students = np.array([[0, 1, 2, 9999, 9999],
+                         [3, 4, 2, 6, 7]])
+
+    assert unique_students(students) == 1
+
+    students = np.array([[0, 1, 2, 9999, 9999],
+                         [3, 4, 5, 6, 7]])
+
+    assert unique_students(students) == 0
+
+def test_unique_projects():
+
+    projects = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+    assert unique_projects(projects) == 0
+
+    projects = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'h']
+
+    assert unique_projects(projects) == 1
+
+
+def test_equality_constraints():
+
+    students = np.array([0, 1, 2, np.nan, 3, 4, 5, 6])
+    projects = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'h']
+
+    np.testing.assert_allclose([0, 1], equality_constraints(students, projects))
+
+
+def test_match_cost():
+
+    choices = {0: ['a', 'b', 'c'],
+               1: ['c', 'd', 'e'],
+               2: ['b', 'c', 'a'],
+               3: ['f', 'g', 'h', 'i'],
+               4: ['a', 'f', 'h'],
+               5: ['c', 'f', 'e'],
+               6: ['c', 'a', 'e']}
+
+    match_counts = {
+                    {0, 1, 2}: {'a': 2, 'b': 2, 'c': 3, 'd': 1, 'e': 1},
+                    {3, 4, 5, 6}: {'a': 2, 'c': 2, 'e': 2, 'f': 3, 'g': 1,
+                                   'h': 2, 'i': 1}
+                   }
+
+    teams = {'c': {0, 1, 2},
+             'f': {3, 4, 5, 6}}
+
+    expected_cost = (3 / 3 + 3 / 4) / 2
+
+    cost = match_cost(choices)
+
+    np.testing.assert_allclose(cost, expected_cost)
+
+
+def test_choose_projects():
+
+    x = [0, 1, 2, np.nan, 3, 4, 5, 6]
+    choices = {0: ['a', 'b', 'c'],
+               1: ['c', 'd', 'e'],
+               2: ['b', 'c', 'a'],
+               3: ['f', 'g', 'h', 'i'],
+               4: ['a', 'f', 'h'],
+               5: ['c', 'f', 'e'],
+               6: ['c', 'a', 'e']}
+    project_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+
+    expected_teams = {'c': {0, 1, 2},
+                      'f': {3, 4, 5, 6}}
+
+    teams = choose_projects(x, choices, project_list)
+
+    assert teams == expected_teams
 
 
 def test_multiple_choice_single_answer_question():
